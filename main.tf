44bd6eba9cf2622f8ba6036569b3e7a938dde09c
neupanes39 <angela@dhangadhi.store>
2025-08-11 12:14:37 +1200

    last part

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+
+    }
+  }
+  cloud {
+
+    organization = "AWS_Learning_git"
+
+    workspaces {
+      name = "handsonlap"
+    }
+  }
+}
+provider "azurerm" {
+
+  features {}
+}
+resource "azurerm_resource_group" "RG" {
+  name     = "813-a8856c5a-hands-on-with-terraform-on-azure"
+  location = "South Central US"
+
+}
+module "storage" {
+  source               = "app.terraform.io/AWS_Learning_git/storage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.RG.location
+  resource_group_name  = azurerm_resource_group.RG
+  storage_account_name = "saangela3567"
+
+}
